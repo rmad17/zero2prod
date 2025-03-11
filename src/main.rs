57320be930085ebcfc1d11919d9603fce0c7aa11
Sourav Basu <souravbasu17@gmail.com>
2025-03-11 23:48:03 +0530

    Added new routers

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    //let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // our router
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health-check", get(health_check));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+// which calls one of these handlers
+async fn health_check() -> String {
+    return "Healthy".to_string();
 }
