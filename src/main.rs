e90a7d615aa3291f2d2a876b3a3c9475092f748b
Sourav Basu <souravbasu17@gmail.com>
2025-03-15 23:53:29 +0530

    Added API Test - WIP

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,12 @@ use axum::{Router, routing::get};
 async fn main() {
     // build our application with a single route
 
+    let router = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health-check", get(health_check));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app()).await.unwrap();
-}
-
-async fn app() -> Router {
-    Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/health-check", get(health_check))
+    axum::serve(listener, router).await.unwrap();
 }
 
 // which calls one of these handlers
@@ -22,36 +19,19 @@ async fn health_check() -> String {
 
 #[cfg(test)]
 mod tests {
-    use axum::Router;
-    use axum::extract::Json;
-    use axum::routing::put;
-    use axum_test::TestServer;
-    use serde_json::Value;
-    use serde_json::json;
-
-    use super::*;
-    use crate::app;
-    //use axum::{
-    //    body::Body,
-    //    extract::connect_info::MockConnectInfo,
-    //    http::{self, Request, StatusCode},
-    //};
 
     #[tokio::test]
-    async fn health_check_succeeds() {
-        let app = app();
-        let server = TestServer::new(app)?;
+    async fn test_health_check_api() {
+        // Assuming your server is running on port 3000
+        let url = "http://0.0.0.0:3000/health-check";
 
-        let response = server
-            .put("/users")
-            .json(&json!({
-                "username": "Terrance Pencilworth",
-            }))
-            .await;
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = response.into_body().collect().await.unwrap().to_bytes();
-        assert_eq!(&body[..], b"Healthy!");
+        //let json_payload = serde_json::json!({"foo": "test"});
+        let response = reqwest::Client::new()
+            .get(url)
+            .send()
+            .await
+            .expect("Expectation!");
+        assert!(response.status().is_success());
+        assert!(response.bytes().await == "Healthy!".to_string());
     }
 }
