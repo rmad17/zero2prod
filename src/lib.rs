8d0b72b4d2d0c3ca46aa0ebea48bf89b314a12e6
Sourav Basu <souravbasu17@gmail.com>
2025-03-22 23:38:19 +0530

    Tests running without recreating router

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ use axum::{Router, routing::get};
 pub async fn run() {
     // build our application with a single route
 
-    let router = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/health-check", get(health_check));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
+}
+
+pub fn app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health-check", get(health_check))
 }
 
 // which calls one of these handlers
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,27 @@
-#[test]
+#[cfg(test)]
 //use axum::{Router, routing::get};
 use axum_test::TestServer;
 
 //use crate::health_check;
+async fn start_app() {
+    let server = zero2prod::run();
+    let _ = tokio::spawn(server);
+}
 
 #[tokio::test]
 async fn test_health_check_api() {
+    start_app().await;
+
     // Assuming your server is running on port 3000
-    //let url = "http://0.0.0.0:3000/health-check";
-    //let router = Router::new().route("/health-check", get(health_check));
-    //let server = TestServer::new(router).unwrap();
-    //
-    ////let json_payload = serde_json::json!({"foo": "test"});
-    //let response = server.get(url).await;
-    //response.assert_status_ok();
-    //let response_text = response.text();
-    //response.assert_text("Healthy!");
-    //assert!(response_text == "Healthy!".to_string());
+    let url = "http://0.0.0.0:3000/health-check";
+
+    let router = zero2prod::app();
+    let server = TestServer::new(router).unwrap();
+
+    //let json_payload = serde_json::json!({"foo": "test"});
+    let response = server.get(url).await;
+    response.assert_status_ok();
+    let response_text = response.text();
+    response.assert_text("Healthy!");
+    assert!(response_text == "Healthy!".to_string());
 }
