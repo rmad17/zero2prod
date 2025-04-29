08fac41f1306eda38ae3cdf4a103793f7ba48e9f
Sourav Basu <souravbasu17@gmail.com>
2025-04-29 07:19:05 +0530

    Fixed migrations and applied them

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -3,39 +3,33 @@ use sea_orm_migration::{prelude::*, schema::*};
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
-//#[async_trait::async_trait]
-//impl MigrationTrait for Migration {
-//    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-//        // Replace the sample below with your own migration scripts
-//        //todo!();
-//        //
-//        //manager
-//        //    .create_table(
-//        //        Table::create()
-//        //            .table(Post::Table)
-//        //            .if_not_exists()
-//        //            .col(pk_auto(Post::Id))
-//        //            .col(string(Post::Title))
-//        //            .col(string(Post::Text))
-//        //            .to_owned(),
-//        //    )
-//        //    .await
-//    }
-//
-//    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-//        // Replace the sample below with your own migration scripts
-//        todo!();
-//
-//        manager
-//            .drop_table(Table::drop().table(Post::Table).to_owned())
-//            .await
-//    }
-//}
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Subscription::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Subscription::Id))
+                    .col(string(Subscription::Name))
+                    .col(string(Subscription::Email))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Subscription::Table).to_owned())
+            .await
+    }
+}
 
 #[derive(DeriveIden)]
-enum Post {
+enum Subscription {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Email,
 }
